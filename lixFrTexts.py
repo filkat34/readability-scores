7cfbbae0659879dc7d3cb12987d5c5d264cf33bd
filippos <filippos29@hotmail.com>
2024-10-27 09:51:27 +0100

    update

---
+++ lixFrTexts.py
@@ -0,0 +1,57 @@
+# LIX readability score calculator for french texts(with Regex)
+# but : calculer l'indice de lisibilité LIX d'un texte français
+# auteur : Filippos
+
+from tkinter import filedialog
+import os
+import re
+
+
+## Fonctions
+
+def nbMots(texte):
+    return len(re.findall(r'\w+', texte))
+
+def nbMotsLongs(texte):
+    return len(re.findall(r'\w\w\w\w\w\w\w+', texte))
+
+def nbPhrases(texte):
+    return len(re.findall(r'\w\w+\s?[.?!]', texte))
+
+def lixScore(nbMots,nbMotsLongs,nbPhrases):
+    return (nbMots/nbPhrases)+(100*(nbMotsLongs/nbMots))
+
+def niveauDifficulte(lixScore):
+    if lixScore > 60 :
+        return "Très difficile"
+    elif lixScore >= 50:
+        return "Difficile"
+    elif lixScore >= 40:
+        return "Moyenne"
+    elif lixScore >= 30:
+        return "Facile"
+    else:
+        return "Très facile"
+
+## Programme
+
+filepath = filedialog.askopenfilename(title= "Chosissez un fichier en texte brut...",
+    filetypes=(("Texte brut", ("*.txt")),("All Files", "*.*")))
+file=open(filepath, encoding="utf8", errors='ignore')
+texte=file.read()
+file.close()
+filename=os.path.basename(filepath)
+nbMots=nbMots(texte)
+nbMotsLongs=nbMotsLongs(texte)
+nbPhrases=nbPhrases(texte)
+lixScore=lixScore(nbMots,nbMotsLongs,nbPhrases)
+niveauDifficulte=niveauDifficulte(lixScore)
+
+print(f'''
+      Fichier.................. {filename}
+      Score LIX................ {lixScore}
+      Difficulté............... {niveauDifficulte}
+      Nombre de mots........... {nbMots}
+      Nombre de mots longs..... {nbMotsLongs}
+      Nombre de phrases........ {nbPhrases}
+      ''')
