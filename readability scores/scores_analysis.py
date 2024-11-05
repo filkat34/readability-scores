4c96b4452d4239e475907532a01ad0899f17134d
filippos <filippos29@hotmail.com>
2024-11-05 20:31:00 +0100

    update

--- readability scores/scores_analysis.py
+++ readability scores/scores_analysis.py
@@ -1,88 +1,97 @@
+"""Grade levels list"""
+niveau = ["CP", "CE1", "CE2", "CM1", "CM2", "Sixième", "Cinquième", "Quatrième", "Troisième",
+           "Seconde", "Première", "Terminale", "Licence", "Master/Doctorat" ]  
+
 def difficulte(score):
+    """Returns the grade level for index scores : ARI, FKGL, Coleman-Liau, Gunning Fog, Smog """
     if score >= 15 :
-        return "Master/doctorat"
+        return niveau[13]
     elif score >= 13 :
-        return "Licence"
+        return niveau [12]
     elif score >= 12 :
-        return "Terminale"
+        return niveau[11]
     elif score >= 11 :
-        return "Première"
+        return niveau[10]
     elif score >= 10 :
-        return "Seconde"
+        return niveau[9]
     elif score >= 9 :
-        return "Troisième"
+        return niveau[8]
     elif score >= 8 :
-        return "Quatrième"
+        return niveau[7]
     elif score >= 7 :
-        return "Cinquième"
+        return niveau[6]
     elif score >= 6 :
-        return "Sixième"
+        return niveau[5]
     elif score >=5 :
-        return "CM2"
+        return niveau[4]
     elif score >= 4 :
-        return "CM1"
+        return niveau[3]
     elif score >= 3 :
-        return "CE2"
+        return niveau[2]
     elif score >= 2 :
-        return "CE1"
+        return niveau[1]
     else:
-        return "CP"
+        return niveau[0]
 
 def difficulte_lix(score):
+    """Returns the grade level for index score : LIX"""
     if score >= 60 :
-        return "Master/doctorat"
+        return niveau[13]
     elif score >= 56 :
-        return "Licence"
+        return niveau[12]
     elif score >= 52 :
-        return "Terminale"
+        return niveau[11]
     elif score >= 48 :
-        return "Première"
+        return niveau[10]
     elif score >= 44 :
-        return "Seconde"
+        return niveau[9]
     elif score >= 40 :
-        return "Troisième"
+        return niveau[8]
     elif score >= 36 :
-        return "Quatrième"
+        return niveau[7]
     elif score >= 32 :
-        return "Cinquième"
+        return niveau[6]
     elif score >= 28 :
-        return "Sixième"
+        return niveau[5]
     elif score >= 24 :
-        return "CM2"
+        return niveau[4]
     elif score >= 20 :
-        return "CM1"
+        return niveau[3]
     elif score >= 15 :
-        return "CE2"
+        return niveau[2]
     elif score >= 10 :
-        return "CE1"
+        return niveau[1]
     else:
-        return "CP"
+        return niveau[0]
     
 
 def difficulte_rix(score):
-    if score >= 7.2 :
-        return "Université"
+    """Returns the grade level for index score : RIX"""
+    if score > 8:
+        return niveau[13]
+    elif score >= 7.2 :
+        return niveau[12]
     elif score >= 6.2 :
-        return "Terminale"
+        return niveau[11]
     elif score >= 5.3 :
-        return "Première"
+        return niveau[10]
     elif score >= 4.5 :
-        return "Seconde"
+        return niveau[9]
     elif score >= 3.7 :
-        return "Troisième"
+        return niveau[8]
     elif score >= 3.0 :
-        return "Quatrième"
+        return niveau[7]
     elif score >= 2.4 :
-        return "Cinquième"
+        return niveau[6]
     elif score >= 1.8 :
-        return "Sixième"
+        return niveau[5]
     elif score >= 1.3 :
-        return "CM2"
+        return niveau[4]
     elif score >= 0.8 :
-        return "CM1"
+        return niveau[3]
     elif score >= 0.5 :
-        return "CE2"
+        return niveau[2]
     elif score >= 0.2 :
-        return "CE1"
+        return niveau[1]
     else:
-        return "CP"
+        return niveau[0]
