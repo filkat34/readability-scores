3221d7a517f917c6c95f67cb19e3b31cc2769a27
filippos <filippos29@hotmail.com>
2024-11-05 18:57:13 +0100

    Update user_input.py

--- readability scores/user_input.py
+++ readability scores/user_input.py
@@ -1,6 +1,7 @@
 from tkinter import filedialog
 
 def user_input():
+    """User input : raw text"""
     filepath = filedialog.askopenfilename(title= "Chosissez un fichier en texte brut...",
     filetypes=[("Texte brut", "*.txt")])
     file=open(filepath, encoding="utf8", errors='ignore')
